6c25119e1ea65918b11671d00f978de714002bbe
Seth Axen <seth@sethaxen.com>
2025-10-21 09:30:39 +0200

    Add and run formatting workflow

--- docs/make.jl
+++ docs/make.jl
@@ -1,17 +1,17 @@
 using DimensionalDistributions
 using Documenter
 
-DocMeta.setdocmeta!(DimensionalDistributions, :DocTestSetup, :(using DimensionalDistributions); recursive=true)
+DocMeta.setdocmeta!(
+    DimensionalDistributions,
+    :DocTestSetup,
+    :(using DimensionalDistributions);
+    recursive=true,
+)
 
 makedocs(;
     modules=[DimensionalDistributions],
     authors="Seth Axen <seth@sethaxen.com> and contributors",
     sitename="DimensionalDistributions.jl",
-    format=Documenter.HTML(;
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    format=Documenter.HTML(; edit_link="main", assets=String[]),
+    pages=["Home" => "index.md"],
 )
