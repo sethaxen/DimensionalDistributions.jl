671d87f2871f8cd47f40333acb9fd5af9ad904c0
Seth Axen <seth@sethaxen.com>
2025-10-21 09:32:36 +0200

    Run formatter

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using JET
         Aqua.test_all(DimensionalDistributions)
     end
     @testset "Code linting (JET.jl)" begin
-        JET.test_package(DimensionalDistributions; target_defined_modules = true)
+        JET.test_package(DimensionalDistributions; target_defined_modules=true)
     end
     # Write your tests here.
 end
