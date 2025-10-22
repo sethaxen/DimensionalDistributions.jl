eff5420055808c5486fede9e90ea1e6505a3722b
Seth Axen <seth@sethaxen.com>
2025-10-22 15:56:55 +0200

    Clean up rand methods

--- src/asdimarraydist.jl
+++ src/asdimarraydist.jl
@@ -118,9 +118,6 @@ function Random.rand!(
 ) where {N}
     return Random.rand!(rng, parent(dist), x)
 end
-function Base.rand(rng::Random.AbstractRNG, dist::AsDimArrayDistribution{0})
-    return rand(rng, parent(dist))
-end
 function Base.rand(rng::Random.AbstractRNG, dist::AsDimArrayDistribution{0}, lengths::Dims)
     return rand(rng, parent(dist), lengths)
 end
@@ -129,15 +126,8 @@ end
 function Base.rand(rng::Random.AbstractRNG, dist::AsDimArrayDistribution)
     return _maybe_dimarray(rand(rng, parent(dist)), Dimensions.dims(dist))
 end
-function Base.rand(
-    rng::Random.AbstractRNG, dist::AsDimArrayDistribution, lengths::Dims
-)
+function Base.rand(rng::Random.AbstractRNG, dist::AsDimArrayDistribution, lengths::Dims)
     dims = Dimensions.dims(dist)
-    xs = map(Base.Fix2(DimensionalData.DimArray, dims), rand(rng, dist, lengths))
+    xs = map(Base.Fix2(DimensionalData.DimArray, dims), rand(rng, parent(dist), lengths))
     return xs
-end
-function Base.rand(
-    rng::Random.AbstractRNG, dist::AsDimArrayDistribution{0}, lengths::Dims
-)
-    return rand(rng, parent(dist), lengths)
 end
