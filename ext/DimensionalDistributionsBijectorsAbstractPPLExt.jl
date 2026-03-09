a29f22d411ac332f6e8e635ef441c9c5cef59e2b
Seth Axen <seth@sethaxen.com>
2026-03-09 21:40:40 +0100

    feat: Implement APPL-dependent part of VectorBijectors interface

---
+++ ext/DimensionalDistributionsBijectorsAbstractPPLExt.jl
@@ -0,0 +1,23 @@
+module DimensionalDistributionsBijectorsAbstractPPLExt
+
+using AbstractPPL: AbstractPPL
+using Bijectors: Bijectors
+using DimensionalDistributions: DimensionalDistributions
+
+@static if isdefined(Bijectors, :VectorBijectors) && isdefined(AbstractPPL, Symbol("@opticof")) 
+    using Bijectors: VectorBijectors
+
+    function VectorBijectors.optic_vec(d::DimensionalDistributions.AbstractDimArrayDistribution)
+        return [AbstractPPL.@opticof(_[i]) for i in Iterators.product(axes(d)...)]
+    end
+
+    function VectorBijectors.linked_optic_vec(d::DimensionalDistributions.AbstractDimArrayDistribution)
+        return VectorBijectors.optic_vec(d)
+    end
+
+    function VectorBijectors.linked_optic_vec(d::DimensionalDistributions.AsDimArrayDistribution)
+        return VectorBijectors.linked_optic_vec(parent(d))
+    end
+end
+
+end  # module
