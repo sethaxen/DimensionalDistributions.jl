0549d4e3e6a704a56eff481370b72e5e48b8e818
Seth Axen <seth@sethaxen.com>
2026-03-09 22:08:27 +0100

    chore: Support DynamicPPL v0.40 (#23)
    * Update DynamicPPL requirement from 0.37, 0.38, 0.39 to 0.37, 0.38, 0.39, 0.40
    
    Updates the requirements on [DynamicPPL](https://github.com/TuringLang/DynamicPPL.jl) to permit the latest version.
    
    Updates `DynamicPPL` to 0.40.7
    - [Release notes](https://github.com/TuringLang/DynamicPPL.jl/releases)
    - [Changelog](https://github.com/TuringLang/DynamicPPL.jl/blob/main/HISTORY.md)
    - [Commits](https://github.com/TuringLang/DynamicPPL.jl/commits/v0.40.7)
    
    ---
    updated-dependencies:
    - dependency-name: DynamicPPL
      dependency-version: 0.40.7
      dependency-type: direct:production
      dependency-group: all-julia-packages
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Update DynamicPPL requirement from 0.37, 0.38, 0.39 to 0.37, 0.38, 0.39, 0.40
    
    Updates the requirements on [DynamicPPL](https://github.com/TuringLang/DynamicPPL.jl) to permit the latest version.
    
    Updates `DynamicPPL` to 0.40.7
    - [Release notes](https://github.com/TuringLang/DynamicPPL.jl/releases)
    - [Changelog](https://github.com/TuringLang/DynamicPPL.jl/blob/main/HISTORY.md)
    - [Commits](https://github.com/TuringLang/DynamicPPL.jl/commits/v0.40.7)
    
    ---
    updated-dependencies:
    - dependency-name: DynamicPPL
      dependency-version: 0.40.7
      dependency-type: direct:production
      dependency-group: all-julia-packages
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix: Only define DPPL code if from_vec_transform available
    
    * feat: Implement VectorBijectors interface
    
    * feat: Implement APPL-dependent part of VectorBijectors interface
    
    * style: Run formatter
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- ext/DimensionalDistributionsDynamicPPLExt.jl
+++ ext/DimensionalDistributionsDynamicPPLExt.jl
@@ -5,44 +5,49 @@ using DimensionalData: DimensionalData, Dimensions
 using DimensionalDistributions: DimensionalDistributions
 using DynamicPPL: DynamicPPL
 
-const ZeroLogProb = zero(float(Real))
+@static if isdefined(DynamicPPL, :from_vec_transform)
+    const ZeroLogProb = zero(float(Real))
 
-struct DimArrayTransform{D} <: Bijectors.Bijector
-    dims::D
-end
+    struct DimArrayTransform{D} <: Bijectors.Bijector
+        dims::D
+    end
 
-(f::DimArrayTransform)(x) = DimensionalData.DimArray(x, f.dims)
+    (f::DimArrayTransform)(x) = DimensionalData.DimArray(parent(x), f.dims)
 
-(::Bijectors.Inverse{<:DimArrayTransform})(x) = x
-Bijectors.with_logabsdet_jacobian(f::DimArrayTransform, x) = (f(x), ZeroLogProb)
-function Bijectors.with_logabsdet_jacobian(::Bijectors.Inverse{<:DimArrayTransform}, x)
-    return (x, ZeroLogProb)
-end
+    (::Bijectors.Inverse{<:DimArrayTransform})(x) = x
+    Bijectors.with_logabsdet_jacobian(f::DimArrayTransform, x) = (f(x), ZeroLogProb)
+    function Bijectors.with_logabsdet_jacobian(::Bijectors.Inverse{<:DimArrayTransform}, x)
+        return (x, ZeroLogProb)
+    end
 
-function DynamicPPL.from_vec_transform(
-    d::DimensionalDistributions.AbstractDimArrayDistribution
-)
-    reshape_transform = DynamicPPL.from_vec_transform_for_size(size(d))
-    dim_array_transform = DimArrayTransform(Dimensions.dims(d))
-    return dim_array_transform ∘ reshape_transform
-end
-function DynamicPPL.from_vec_transform(d::DimensionalDistributions.AsDimArrayDistribution)
-    return DimArrayTransform(Dimensions.dims(d)) ∘ DynamicPPL.from_vec_transform(parent(d))
-end
+    function DynamicPPL.from_vec_transform(
+        d::DimensionalDistributions.AbstractDimArrayDistribution
+    )
+        reshape_transform = DynamicPPL.from_vec_transform_for_size(size(d))
+        dim_array_transform = DimArrayTransform(Dimensions.dims(d))
+        return dim_array_transform ∘ reshape_transform
+    end
+    function DynamicPPL.from_vec_transform(
+        d::DimensionalDistributions.AsDimArrayDistribution
+    )
+        return DimArrayTransform(Dimensions.dims(d)) ∘
+               DynamicPPL.from_vec_transform(parent(d))
+    end
 
-function DynamicPPL.from_linked_vec_transform(
-    d::DimensionalDistributions.AbstractDimArrayDistribution
-)
-    f_link = Bijectors.bijector(d)
-    reshape_transform = DynamicPPL.from_vec_transform(f_link, size(d))
-    dim_array_transform = DimArrayTransform(Dimensions.dims(d))
-    return dim_array_transform ∘ Bijectors.inverse(f_link) ∘ reshape_transform
-end
-function DynamicPPL.from_linked_vec_transform(
-    d::DimensionalDistributions.AsDimArrayDistribution
-)
-    return DimArrayTransform(Dimensions.dims(d)) ∘
-           DynamicPPL.from_linked_vec_transform(parent(d))
+    function DynamicPPL.from_linked_vec_transform(
+        d::DimensionalDistributions.AbstractDimArrayDistribution
+    )
+        f_link = Bijectors.bijector(d)
+        reshape_transform = DynamicPPL.from_vec_transform(f_link, size(d))
+        dim_array_transform = DimArrayTransform(Dimensions.dims(d))
+        return dim_array_transform ∘ Bijectors.inverse(f_link) ∘ reshape_transform
+    end
+    function DynamicPPL.from_linked_vec_transform(
+        d::DimensionalDistributions.AsDimArrayDistribution
+    )
+        return DimArrayTransform(Dimensions.dims(d)) ∘
+               DynamicPPL.from_linked_vec_transform(parent(d))
+    end
 end
 
 end  # module
